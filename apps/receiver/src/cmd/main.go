43046bd92c238382333d8446f6a6527e3d962723
ysuz032 <102312063+ysuz032@users.noreply.github.com.>
2024-06-15 09:38:58 +0000

    mod the way interface names are passed to programs

--- apps/receiver/src/cmd/main.go
+++ apps/receiver/src/cmd/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"receiver/pkg/receiver"
 	"sync"
+
+	"receiver/pkg/receiver"
 )
 
 func main() {
 	// 環境変数からアドレスとポートを取得
 	multicastAddr := os.Getenv("MULTICAST_ADDR")
 	multicastPort := os.Getenv("MULTICAST_PORT")
-	multicastIface := os.Getenv("MULTICAST_INTERFACE")
+
+	// 引数からインターフェース名を取得
+	multicastIface := flag.String("iface", "", "network interface to use for multicast")
+	flag.Parse()
 
 	if multicastAddr == "" || multicastPort == "" {
 		fmt.Println("Not found MULTICAST_ADDR OR MULTICAST_PORT in environment variables")
@@ -25,7 +30,7 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		receiver.ReceiveMulticast(multicastIface, multicastAddr, multicastPort)
+		receiver.ReceiveMulticast(*multicastIface, multicastAddr, multicastPort)
 	}()
 
 	// 全てのゴルーチンの終了を待つ
